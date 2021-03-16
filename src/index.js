fd1b3b9deea78de38a51ce5f77bd784e54886356
CHNUTT <65862558+CHNUTT@users.noreply.github.com>
2021-03-15 22:38:02 -0500

    [Database Setup] - set up postgres database connection

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
+const appName = require('./../package').name;
 const express = require('express');
 const cors = require('cors');
+const log4js = require('log4js');
 const app = express();
+
+const logger = log4js.getLogger(appName);
+logger.level = process.env.LOG_LEVEL || 'info';
 
 // INFO Register middlewares
 app.use(cors());
@@ -13,8 +18,23 @@ app.use('/status', (req, res, next) => {
 
 const PORT = process.env.PORT || 3000;
 
-const init = function () {
-  app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+const init = async function () {
+  try {
+    const db = require('./models/index');
+    console.log(db);
+    const connection = await db();
+    if (!connection) {
+      throw new Error('Connection to DB errors');
+    }
+    logger.info('is successfully connected to the DB');
+
+    const port = process.env.PORT || 3000;
+    app.listen(port, function () {
+      logger.info(`emperor-backend listening on http://localhost:${port}`);
+    });
+  } catch (err) {
+    throw err;
+  }
 };
 
 init();
